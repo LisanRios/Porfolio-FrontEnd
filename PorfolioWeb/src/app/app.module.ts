e03f9192744f862f6bab922f96a1cc168b7f98cf
Lisandro Rios <rios.lisandro369@gmail.com>
2022-12-27 15:15:17 -0300

    angular update 3

--- PorfolioWeb/src/app/app.module.ts
+++ PorfolioWeb/src/app/app.module.ts
@@ -2,18 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
-import { MisProyectosComponent } from './componentes/mis-proyectos/mis-proyectos.component';
-import { ContactoComponent } from './componentes/contacto/contacto.component';
-import { HeadComponent } from './componentes/head/head.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    EncabezadoComponent,
-    MisProyectosComponent,
-    ContactoComponent,
-    HeadComponent,
+
   ],
   imports: [
     BrowserModule
