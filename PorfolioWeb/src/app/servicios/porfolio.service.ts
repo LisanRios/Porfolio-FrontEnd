52d6c0f61d9e81eeb32a2638f7ea5656a0dfc7c0
Lisandro Rios <rios.lisandro369@gmail.com>
2022-12-27 14:50:57 -0300

    angular update

---
+++ PorfolioWeb/src/app/servicios/porfolio.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PorfolioService {
+
+  constructor() { }
+
+  obtenerDatos(){
+    console.log("el servicio porfolio esta funcinado");
+  }
+
+}
