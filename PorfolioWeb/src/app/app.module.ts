70b546ec50c46c903d49efdf9148c7ad93ef246b
Lisandro Rios <rios.lisandro369@gmail.com>
2022-12-28 19:45:44 -0300

    angular update 10

--- PorfolioWeb/src/app/app.module.ts
+++ PorfolioWeb/src/app/app.module.ts
@@ -3,20 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
-import { LalineaComponent } from './componentes/lalinea/lalinea.component';
 import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
 import { ContactoComponent } from './componentes/contacto/contacto.component';
-
+import { HttpClientModule } from "@angular/common/http";
 @NgModule({
   declarations: [
     AppComponent,
     EncabezadoComponent,
-    LalineaComponent,
     ProyectosComponent,
     ContactoComponent,
   ],
   imports: [
     BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- PorfolioWeb/src/app/componentes/proyectos/proyectos.component.ts
+++ PorfolioWeb/src/app/componentes/proyectos/proyectos.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PorfolioService } from 'src/app/servicios/porfolio.service';
 
 @Component({
   selector: 'app-proyectos',
   templateUrl: './proyectos.component.html',
   styleUrls: ['./proyectos.component.css']
 })
-export class ProyectosComponent {
+export class ProyectosComponent implements OnInit {
+  projectList:any;
+  tecnologyList:any;
+  constructor(private datosPorfolio:PorfolioService ) {
+  
+  }
 
+  ngOnInit(): void {
+    this.datosPorfolio.obtenerDatos().subscribe(data=> {
+      this.projectList = data.project;
+    });
+    this.datosPorfolio.obtenerDatos().subscribe(data=> {
+      this.tecnologyList = data.tecnology;
+    });
+  }
 }
