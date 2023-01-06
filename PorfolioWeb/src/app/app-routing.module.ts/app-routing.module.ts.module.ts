6f6617d0d428f25090e5ce554ba6b8802bee9fac
Lisandro Rios <rios.lisandro369@gmail.com>
2023-01-06 17:59:39 -0300

    angular update 16

--- PorfolioWeb/src/app/app-routing.module.ts/app-routing.module.ts.module.ts
+++ PorfolioWeb/src/app/app-routing.module.ts/app-routing.module.ts.module.ts
@@ -7,12 +7,15 @@ import { EncabezadoComponent } from '../componentes/encabezado/encabezado.compon
 import { ProyectosComponent } from '../componentes/proyectos/proyectos.component';
 import { ContactoComponent } from '../componentes/contacto/contacto.component';
 import { TecnologiasComponent } from '../componentes/tecnologias/tecnologias.component';
+import { Pagina404Component } from '../componentes/pagina404/pagina404.component';
 
 const routes: Routes = [
+  {path: '', redirectTo: 'Inicio', pathMatch: 'full'},
   {path: 'Inicio', component: EncabezadoComponent},
   {path: 'Proyectos', component: ProyectosComponent},
   {path: 'Tecnologías-Dominadas', component: TecnologiasComponent},
   {path: 'Contacto', component: ContactoComponent},  
+  {path: '**', component: Pagina404Component},
 ];
 
 
--- PorfolioWeb/src/app/app.module.ts
+++ PorfolioWeb/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { ContactoComponent } from './componentes/contacto/contacto.component';
 import { HttpClientModule } from "@angular/common/http";
 import { AppRoutingModuleTsModule } from './app-routing.module.ts/app-routing.module.ts.module';
 import { TecnologiasComponent } from './componentes/tecnologias/tecnologias.component';
+import { Pagina404Component } from './componentes/pagina404/pagina404.component';
 
 
 
@@ -19,6 +20,7 @@ import { TecnologiasComponent } from './componentes/tecnologias/tecnologias.comp
     ProyectosComponent,
     ContactoComponent,
     TecnologiasComponent,
+    Pagina404Component,
   ],
   imports: [
     BrowserModule,
